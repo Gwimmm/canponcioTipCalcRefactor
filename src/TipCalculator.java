2ee90f36394a587bbea7db13d797594a97585d39
BT_3W2_13 <ronaldoc25@nycstudents.net>
2022-12-16 09:24:51 -0500

    completed(?)

--- src/TipCalculator.java
+++ src/TipCalculator.java
@@ -1,2 +1,38 @@
 public class TipCalculator {
+    private int people;
+    private double tipPercent;
+    private double totalBillBeforeTip;
+
+    public TipCalculator (int people, double tipPercent){
+        this.people = people;
+        this.tipPercent = tipPercent;
+        this.totalBillBeforeTip = 0.0;
+    }
+    public double getTotalBillBeforeTip(){
+        return totalBillBeforeTip;
+    }
+    public double getTipPercentage(){
+        return tipPercent;
+    }
+    public double addMeal(double cost){
+        totalBillBeforeTip = totalBillBeforeTip + cost;
+        return totalBillBeforeTip;
+    }
+    public double tipAmount(){
+        return (double)(tipPercent * totalBillBeforeTip);
+    }
+    public double totalBill(){
+        return totalBillBeforeTip + tipAmount();
+    }
+    public double perPersonCostBeforeTip(){
+        return totalBillBeforeTip / people;
+    }
+    public double perPersonTipAmount(){
+        return tipAmount() / people;
+    }
+    public double perPersonTotalCost(){
+        return totalBill() / people;
+    }
+
+
 }
--- src/TipCalculatorRunner.java
+++ src/TipCalculatorRunner.java
@@ -1,5 +1,27 @@
-public class TipCalculatorRunner {
+import java.util.Scanner;
+public class TipCalculatorRunner
+{
     public static void main(String[] args) {
-
+        Scanner input = new Scanner(System.in); //creates a new scanner
+        System.out.println("Welcome to the tip calculator!");
+        System.out.println("How many people are in your group?");
+        int ppl = input.nextInt();
+        System.out.println("What's the tip percentage? (0-100)");
+        double tip = input.nextDouble();
+        TipCalculator hunrgy = new TipCalculator(ppl, tip);
+        double mealCost = 0;
+        while (!(mealCost == -1)) {
+            System.out.println("Enter a cost in dollars and cents, e.g. 12.50 (type -1 to end)");
+            mealCost = input.nextDouble();
+            hunrgy.addMeal(mealCost);
+        }
+        System.out.println("^^^^^^^^^^^^^^^^^^^^^^^^^^");
+        System.out.printf("Total Bill Before Tip: $%.2f\n", hunrgy.getTotalBillBeforeTip());
+        System.out.println("Tip Percentage: "+hunrgy.getTipPercentage());
+        System.out.printf("Total Tip: $%.2f\n", hunrgy.tipAmount());
+        System.out.printf("Total Bill With Tip: $%.2f\n", hunrgy.totalBill());
+        System.out.printf("Per Person Cost Before Tip: $%.2f\n", hunrgy.perPersonCostBeforeTip());
+        System.out.printf("Tip Per Person: $%.2f\n", hunrgy.perPersonTipAmount());
+        System.out.printf("Total Cost Per Person: $%.2f\n", hunrgy.perPersonTotalCost());
     }
 }
